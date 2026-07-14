d0382aa44c73792b18509ad2eb20ed6a303ef860
Noor ul Haq <157638741+noorulhaq-cs@users.noreply.github.com>
2026-07-14 16:56:44 +0500

    Configure local dev environment

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Yeh lines add karein jo fonts download ko bypass karein:
-  experimental: {
-    optimizeFonts: false,
-  } as any,
 };
 
 export default nextConfig;
